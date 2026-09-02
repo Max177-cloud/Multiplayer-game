ff11cc44912d3dede652d0ccd6a0dccfbecaeb73
Max177-cloud <starodubmax60@gmail.com>
2026-09-02 18:01:41 +0300

    made an API example

--- server/server.js
+++ server/server.js
@@ -4,8 +4,16 @@ const { createServer } = require('node:http');
 const app = express();
 const server = createServer(app);
 
+const users = [{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }];
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.send('<h1>Multiplayer API v1.0.0</h1>');
+});
+app.get('/test', (req, res) => {
+  res.send('<h1>Hello test</h1>');
+});
+app.get('/users', (req, res) => {
+  res.json(users);
 });
 
 server.listen(3000, () => {
